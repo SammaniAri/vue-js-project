e0cd6958485cfc3ef1acf58a99527b10d1088585
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-07-04 17:19:39 +0200

    Adding the card acomponent

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,29 @@
+<!-- @format -->
+<!-- @format -->
+
+<template>
+	<div class="card p-2">
+		<div
+			class="container flex items-center flex-col bg-[#a1a1aa] w-64 h-64 rounded-lg p-4">
+			<div
+				class="picture rounded-full bg-[#71717a] w-24 h-24 m-2">
+			</div>
+			<div class="p-2">
+				<strong>
+					First <span> Last</span>
+				</strong>
+			</div>
+			<button
+				class="rounded-full bg-[#71717a] opacity-25 hover:bg-sky-700 w-24">
+				Contact</button
+			>
+		</div>
+	</div>
+</template>
+
+<script></script>
+<style>
+h1 {
+	margin: 40px 0 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <!-- @format -->
 
-<template>Home </template>
-<script setup></script>
+<template>
+	<div class="flex flex-wrap my-8">
+		<CardComponent /> <CardComponent />
+	</div>
+</template>
+<script setup>
+import CardComponent from "../components/CardComponent.vue";
+</script>
 <style scoped></style>
