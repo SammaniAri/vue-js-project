b373af100aa76a5bf4bcf8d04f9b7eac9a3137da
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-07-05 18:39:05 +0200

    passing api data into the cardcomponent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,35 +4,11 @@
 	<div
 		class="flex flex-wrap my-8 gap-1 mx-16">
 		<CardComponent
-			:firstName="'Sansa'"
-			:lastName="'Stark'"
-			:profilePicture="'https://i.pinimg.com/736x/71/31/de/7131deb4cc193c21af260e6effcf73c1.jpg'"
-			:contactInfo="' contact'" />
-		<CardComponent
-			:firstName="'Ned'"
-			:lastName="'Stark'"
-			:profilePicture="'https://i.pinimg.com/736x/71/31/de/7131deb4cc193c21af260e6effcf73c1.jpg'"
-			:contactInfo="' contact'" />
-		<CardComponent
-			:firstName="'Grey'"
-			:lastName="'Worm'"
-			:profilePicture="'https://i.pinimg.com/736x/71/31/de/7131deb4cc193c21af260e6effcf73c1.jpg'"
-			:contactInfo="' contact'" />
-		<CardComponent
-			:firstName="'Tyrion'"
-			:lastName="'Lannister'"
-			:profilePicture="' '"
-			:contactInfo="' contact'" />
-		<CardComponent
-			:firstName="'Arya'"
-			:lastName="'Stark'"
-			:profilePicture="' '"
-			:contactInfo="' contact'" />
-		<CardComponent
-			:firstName="'Jamie'"
-			:lastName="'Lannister'"
-			:profilePicture="' '"
-			:contactInfo="' contact'" />
+			v-for="employee of employeeInformation"
+			:firstName="employee.first_name"
+			:lastName="employee.last_name"
+			:profilePicture="employee.avatar"
+			:contactInfo="employee.email" />
 	</div>
 </template>
 <script setup>
@@ -40,13 +16,14 @@ import { ref, onMounted } from "vue";
 import CardComponent from "../components/CardComponent.vue";
 import axiosClient from "../views/axiosClient.js";
 
-const firstName = ref([]);
-
+let employeeInformation = ref([]);
 onMounted(async () => {
 	const response =
 		await axiosClient.get("/");
 	console.log(response.data.data);
-	firstName.value = response.data.data;
+
+	employeeInformation.value =
+		response.data.data;
 });
 </script>
 <style scoped></style>
