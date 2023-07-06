c576ecabb5f14445d1484e979124e452a24cb2a8
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-07-06 11:00:43 +0200

    Added pagination UI

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,45 @@
+<!-- @format -->
+
+<script></script>
+<template
+	><nav aria-label="Page navigation">
+		<ul class="list-style-none flex">
+			<li>
+				<a
+					class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+					href="#"
+					aria-label="Previous">
+					<span aria-hidden="true"
+						>&laquo;</span
+					>
+				</a>
+			</li>
+			<li>
+				<a
+					class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+					href="#"
+					>1</a
+				>
+			</li>
+
+			<li>
+				<a
+					class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+					href="#"
+					>2</a
+				>
+			</li>
+			<li>
+				<a
+					class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+					href="#"
+					aria-label="Next"
+					><span aria-hidden="true"
+						>&raquo;</span
+					>
+				</a>
+			</li>
+		</ul>
+	</nav></template
+>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,11 +9,15 @@
 			:lastName="employee.last_name"
 			:profilePicture="employee.avatar"
 			:contactInfo="employee.email" />
+		<div>
+			<PaginationComponent />
+		</div>
 	</div>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
 import CardComponent from "../components/CardComponent.vue";
+import PaginationComponent from "../components/PaginationComponent.vue";
 import axiosClient from "../views/axiosClient.js";
 
 let employeeInformation = ref([]);
